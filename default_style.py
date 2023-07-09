1b504a8cc1c095815fedb0628b1219e6e85f1e55
djanloo <zzibecu@gmail.com>
2023-07-09 16:34:52 +0200

    minor

--- default_style.py
+++ default_style.py
@@ -35,3 +35,5 @@ plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+
+print("font sizes set")
